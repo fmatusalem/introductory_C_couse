fe778b4c0f87ffb900c9f7edacf18e06ad4c1a72
VASP, SIESTA, CP2K  post processing tools <fmatusalem@yahoo.com.br>
2022-04-21 14:54:39 -0300

    Add files via upload

---
+++ sum_two_float_numbers/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+float i,j;
+
+printf("Input the first number: ");
+scanf("%f",&i);
+
+printf("Input the second number: ");
+scanf("%f",&j);
+
+printf("i =  %5.3f\nj = %5.3f\n\n",i,j);
+
+
+
+printf("Sum of the above two numbers = %4.2f\n",i+j);
+    return 0;
+}
