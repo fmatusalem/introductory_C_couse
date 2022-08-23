83a84d4f4854ac056e9926463c6b853f3f22c47c
VASP, SIESTA, CP2K  post processing tools <fmatusalem@yahoo.com.br>
2022-08-23 12:28:05 -0300

    Update print_name.c

--- print_name.c
+++ print_name.c
@@ -8,5 +8,6 @@ printf("Name : Alexandra Abramov\n");
 printf("DOB : July 14, 1975\n");
 printf("Mobile : 99-9999999999\n");
 
-
+system("pause");  //to avoid the program to close instantly
+return 0;
 }
